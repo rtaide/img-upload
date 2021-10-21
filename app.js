a8df25dfbc3af5a3f705d74da6f16fb44aa010fa
Rachit Taiden <rachit.taide.551832@gmail.com>
2021-10-21 16:55:00 +0530

    initial commit

--- app.js
+++ app.js
@@ -21,35 +21,6 @@ mongoose.connect('mongodb://localhost:27017/Images',{
     console.log("err0r while connecting")
 })
 
-//Schema
-/*var imgSchema = mongoose.Schema({
-    img:{data:Buffer,contentType: String}
-});
-var image = mongoose.model("image",imgSchema);*/
-// SET STORAGE
-
-  //Routes
-/*app.get("/",(req,res)=>{
-    res.render("index");
-});*/
-/*app.post("/uploadphoto",upload.single('myImage'),(req,res)=>{
-    var img = fs.readFileSync(req.file.path);
-    var encode_img = img.toString('base64');
-    var final_img = {
-        contentType:req.file.mimetype,
-        image:new Buffer (encode_img,'base64')
-    };
-    image.create(final_img,function(err,result){
-        if(err){
-            console.log(err);
-        }else{
-            console.log(result.img.Buffer);
-            console.log("Saved To database");
-            res.contentType(final_img.contentType);
-            res.send(final_img.image);
-        }
-    })
-})*/
 
 app.listen(3000,function () {
       console.log("Server Started at PORT 3000");
